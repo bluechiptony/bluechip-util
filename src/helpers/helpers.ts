58ad803098dd7216db943682a3c371c86a22f363
bluechiptony <tony5egwu@gmail.com>
2020-09-02 11:47:51 +0100

    neat csv install and read csv function

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,4 +1,19 @@
 import * as CamelCaseKeys from 'camelcase-keys';
+import { readFileSync, createReadStream } from 'fs';
+const neatCsv = require('neat-csv');
+
+export const readCsvFile = async (url: string): Promise<Object[]> => {
+  try {
+    let fileData = await readFileSync(url).toString();
+    if (fileData.charCodeAt(0) === 0xfeff) {
+      fileData = fileData.substr(1);
+    }
+    let parsed = await neatCsv(fileData);
+    return parsed;
+  } catch (error) {
+    throw new Error('Error parsing file');
+  }
+};
 
 export const adaptExpressRequest = (req: any): AppAdaptedRequest => {
   const adaptRequest: AppAdaptedRequest = {
