717e6ead9ca3871d1515c2842faba56d3515af89
Anthony Egwu <tony5egwu@gmail.com>
2020-10-18 10:35:01 +0100

    export functions from members

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
 export * as BlueChipValidators from './validators/validators';
+export {
+  validateEmailAddress,
+  validateIfArray,
+  validatePhoneNumber,
+  validateRequiredDateProperty,
+  validateRequiredNumericProperty,
+  validateRequiredProperty,
+  validateRequiredStringProperty,
+} from './validators/validators';
 export * as BluechipHelpers from './helpers/helpers';
+export {
+  readCsvFile,
+  adaptExpressRequest,
+  upperFirstCharacter,
+  getDateStringFromTime,
+  isValidEmail,
+  isValidPhoneNumber,
+  camelizeCase,
+  convertToNameCase,
+  convertWordToSentenceCase,
+  AppAdaptedRequest,
+} from './helpers/helpers';
 export * from './responses/responses';
 export { DatabaseError } from './exceptions/database';
 export { InvalidDataTypeError } from './exceptions/data';
